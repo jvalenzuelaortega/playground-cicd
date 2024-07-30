30b5c8960a2e0336a0fd3e7268225e61decbf908
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2024-07-29 22:54:57 -0400

    FEATURE: add example logs

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/demo")
     public ResponseEntity<?> getDemoResponse() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return ResponseEntity.ok("Hello World");
     }
 }
