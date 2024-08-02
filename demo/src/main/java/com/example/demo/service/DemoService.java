64afe2638457520ed78874d8fc854ee972f54d04
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2024-08-01 22:16:57 -0400

    FEATURE: add example services class

---
+++ demo/src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String getMessage(String message) {
+        return "Hello".concat(message);
+    }
+}
